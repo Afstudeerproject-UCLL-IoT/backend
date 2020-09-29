f97bd6e3a9e607de818645ac8e2fcd456fd92ecc
Indy Naessens <indy.naessens@pm.me>
2020-09-29 11:59:37 +0200

    Domain changes needed for tests
    Test:
    - 155645f41580182f02d4ab4dbe93374df3ed012a
    - b3438c98e5d8b3d78f193cac0d5c460a06f975c8
    - 764626ccb45d02c2f46c5ac2df5f3c9ef3cf67b8

--- src/core/src/main/java/core/domain/Event.java
+++ src/core/src/main/java/core/domain/Event.java
@@ -3,5 +3,7 @@ package core.domain;
 public enum Event {
     REGISTER_DEVICE,
     PUZZLE_COMPLETED,
-    PUZZLE_SUBSCRIPTION
+    PUZZLE_SUBSCRIPTION,
+    GAME_STARTED,
+    GAME_ENDED
 }
---
+++ src/core/src/main/java/core/domain/GameSession.java
@@ -0,0 +1,86 @@
+package core.domain;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class GameSession {
+    private final long id;
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+
+    private GameSession(long id, LocalDateTime start, LocalDateTime end) {
+        this.id = id;
+        this.start = start;
+        this.end = end;
+    }
+
+    // getters
+    public long getId() {
+        return id;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    // overrides
+    @Override
+    public String toString() {
+        var startFormat = start.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        var endFormat = end.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        return String.format("Game session %d: %s-%s", id, startFormat, endFormat);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof GameSession){
+            var other = (GameSession)o;
+            return other.getId() == getId() &&
+                    other.getStart().equals(getStart()) &&
+                    other.getEnd().equals(getEnd());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getStart(), getEnd());
+    }
+
+    // builder
+    public static class Builder {
+        private long id;
+        private LocalDateTime start;
+        private LocalDateTime end;
+
+        public Builder withId(long id){
+            this.id = id;
+            return this;
+        }
+
+        public Builder withoutId(){
+            this.id = 0;
+            return this;
+        }
+
+        public Builder withStartTime(LocalDateTime start){
+            this.start = start;
+            return this;
+        }
+
+        public Builder withEndTime(LocalDateTime end){
+            this.end = end;
+            return this;
+        }
+
+        public GameSession build(){
+            return new GameSession(id, start, end);
+        }
+    }
+}
