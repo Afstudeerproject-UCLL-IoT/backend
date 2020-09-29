155645f41580182f02d4ab4dbe93374df3ed012a
Indy Naessens <indy.naessens@pm.me>
2020-09-29 10:53:40 +0200

    Create game tests
    Needed for #17

---
+++ src/core/src/test/java/core/usecases/game/GameUseCasesCreateGameTest.java
@@ -0,0 +1,48 @@
+package core.usecases.game;
+
+import core.domain.Game;
+import core.exceptions.game.GameAlreadyExistsException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class GameUseCasesCreateGameTest extends GameUseCasesBase {
+
+    @Test
+    public void newGameCanBeCreated(){
+        var game = new Game.Builder()
+                .withName("Game1")
+                .build();
+
+        var success = gameUseCases.createGame(game);
+        verify(gameRepository).add(any(Game.class));
+
+        assertTrue(success);
+    }
+
+    @Test
+    public void duplicateGameThrowsException(){
+        // stub
+        when(gameRepository.exists(any(Game.class)))
+                .thenReturn(true);
+
+        // duplicate game
+        var duplicate = new Game.Builder()
+                .withName("Game1")
+                .build();
+
+        assertThrows(GameAlreadyExistsException.class, () -> gameUseCases.createGame(duplicate));
+        verify(gameRepository).exists(any(Game.class));
+        verify(gameRepository, never()).add(any(Game.class));
+    }
+
+    @Test
+    public void nullGameExitsEarly(){
+        assertThrows(IllegalArgumentException.class, () -> gameUseCases.createGame(null));
+        verify(gameRepository, never()).exists(any(Game.class));
+        verify(gameRepository, never()).add(any(Game.class));
+    }
+}
