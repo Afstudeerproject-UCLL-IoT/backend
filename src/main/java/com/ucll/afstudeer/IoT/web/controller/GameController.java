ab3ea89fac9298213544c3de0089c3122dbf01db
Indy Naessens <indy.naessens@pm.me>
2020-10-07 14:02:25 +0200

    Start game endpoint
    Closes #19

--- src/main/java/com/ucll/afstudeer/IoT/web/controller/GameController.java
+++ src/main/java/com/ucll/afstudeer/IoT/web/controller/GameController.java
@@ -1,6 +1,7 @@
 package com.ucll.afstudeer.IoT.web.controller;
 
 import com.ucll.afstudeer.IoT.domain.Game;
+import com.ucll.afstudeer.IoT.domain.GameSession;
 import com.ucll.afstudeer.IoT.domain.PuzzleSubscription;
 import com.ucll.afstudeer.IoT.dto.out.GameWithPuzzlesDto;
 import com.ucll.afstudeer.IoT.service.game.GameService;
@@ -33,9 +34,25 @@ public class GameController {
 
     @PostMapping("/{gameName}")
     public boolean addPuzzleSubscriptions(@PathVariable String gameName, @RequestBody List<PuzzleSubscription> subscriptions){
-        var game = new Game.Builder().withName(gameName).build();
+        // create game
+        var game = new Game.Builder()
+                .withName(gameName)
+                .build();
 
+        // add puzzle subscriptions
         return gameService.addPuzzleSubscriptions(game, subscriptions)
+                .getValue();
+    }
+
+    @GetMapping("/{gameName}/start")
+    public GameSession startGame(@PathVariable String gameName){
+        // create game
+        var game = new Game.Builder()
+                .withName(gameName)
+                .build();
+
+        // start game
+        return gameService.startGame(game)
                 .getValue();
     }
 }
